34890178e0bb3be1732adc506ef3647839636b64
nazhexa1 <31029921+nazhexa1@users.noreply.github.com>
2017-08-16 07:03:15 +0000

    script is modified

--- pari1.ps1
+++ pari1.ps1
@@ -1,7 +1,7 @@
 Import-Module WebAdministration
 $iisAppPoolName = "WebApplication10"
 $iisAppPoolDotNetVersion = "v4.0"
-$iisAppName = "WebApplication800"
+$iisAppName = "WebApplication400"
 $directoryPath = "C:\inetpub\wwwroot\WebApplication10"
 
 #navigate to the app pools root
