6eb4f21580da38bea75782adcaf3f4bc2788d312
naz <naziaparveen10@gmail.com>
2017-08-16 06:46:42 +0000

    script is modified

---
+++ pari1.ps1
@@ -0,0 +1,29 @@
+Import-Module WebAdministration
+$iisAppPoolName = "WebApplication10"
+$iisAppPoolDotNetVersion = "v4.0"
+$iisAppName = "WebApplication800"
+$directoryPath = "C:\inetpub\wwwroot\WebApplication10"
+
+#navigate to the app pools root
+cd IIS:\AppPools\
+
+#check if the app pool exists
+if (!(Test-Path $iisAppPoolName -pathType container))
+{
+    #create the app pool
+    $appPool = New-Item $iisAppPoolName
+    $appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+}
+
+#navigate to the sites root
+cd IIS:\Sites\
+
+#check if the site exists
+if (Test-Path $iisAppName -pathType container)
+{
+    return
+}
+
+#create the site
+$iisApp = New-Item $iisAppName -bindings @{protocol="http";bindingInformation=":90:"} -physicalPath $directoryPath
+$iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName
